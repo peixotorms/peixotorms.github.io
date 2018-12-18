d80b7356bd7ccb134c6d1a5243c02f292d944a28
Raul Peixoto <raul.peixoto@gmail.com>
2018-12-18 09:36:07 +0700

    Create fingerprint.js

---
+++ tests/PSI/fingerprint.js
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>PSI JavaScript UA Test</title>
+<style>
+body { font-family: Arial, Helvetica, sans-serif; line-height: 38px; } 
+#ua { font-size: 28px; font-weight: bold; color: #FF0000; } 
+</style>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/fingerprintjs2/2.0.3/fingerprint2.min.js"></script>
+<script>
+document.addEventListener("DOMContentLoaded", function(){
+	if (window.requestIdleCallback) {
+		requestIdleCallback(function () {
+			Fingerprint2.get(function (components) {
+			  console.log(components); // an array of components: {key: ..., value: ...}
+			  document.getElementById("ua").innerHTML = "UA 1: " + JSON.stringify(components);
+			})
+		})
+	} else {
+		setTimeout(function () {
+			Fingerprint2.get(function (components) {
+			  console.log(components); // an array of components: {key: ..., value: ...}
+			  document.getElementById("ua").innerHTML = "UA 2: " + JSON.stringify(components);
+			})  
+		}, 500)
+	}
+});
+</script>
+
+</head>
+<body>
+<div id="ua">...</div>
+<body>
+</html>
